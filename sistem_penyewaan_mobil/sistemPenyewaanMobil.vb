e5c423a5a60fa84f7ec32f007b0311f1031c372e
mythashbr <mitaashbra@gmail.com>
2022-01-18 23:07:05 +0700

    Fix

--- sistem_penyewaan_mobil/sistemPenyewaanMobil.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class sistemPenyewaanMobil
-    Private Sub sistemPenyewaanMobil_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-    End Sub
-End Class
